83011e208097d3a6ef2ec402e6c8ae03ae40e7f0
danydf1 <49538985+danydf1@users.noreply.github.com>
2022-08-25 18:33:08 -0300

    primer commit
    clase empleado y profesor

---
+++ src/Dominio/Empleado.java
@@ -0,0 +1,52 @@
+package Dominio;
+
+public class Empleado {
+	// declaracion de variables
+		private int id;
+		private String nombre;
+		private int edad;
+		static int idIni = 999;
+		
+	// creacion de constructores	
+		public Empleado() {
+			idIni++;
+			this.id = idIni; 
+			this.edad = 99;
+			this.nombre = "sin nombre";
+		}
+		public Empleado(String nombre, int edad) {
+			idIni++;
+			this.id = idIni;
+			this.nombre = nombre;
+			this.edad = edad;
+		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+		public int getEdad() {
+			return edad;
+		}
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+		
+		@Override
+		public String toString() {
+			return "Empleado [id=" + id + ", nombre=" + nombre + ", edad=" + edad + "]";
+		}
+		
+		
+		public static int DevuelveProximoID() {
+			//la clase devolvera el proximo ID
+			return idIni+1;
+		}
+}
---
+++ src/Dominio/Profesor.java
@@ -0,0 +1,38 @@
+package Dominio;
+
+public class Profesor extends Empleado{
+	private String Cargo;
+	private int AntiguedadDocente;
+
+
+	public Profesor() {
+		super();
+	}
+	
+	public Profesor(String nombre, int edad, String cargo, int antiguedadDocente) {
+		super(nombre, edad);
+		this.Cargo = cargo;
+		this.AntiguedadDocente = antiguedadDocente;
+	}
+
+	public String getCargo() {
+		return Cargo;
+	}
+
+	public void setCargo(String cargo) {
+		Cargo = cargo;
+	}
+
+	public int getAntiguedadDocente() {
+		return AntiguedadDocente;
+	}
+
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		AntiguedadDocente = antiguedadDocente;
+	}
+
+	@Override
+	public String toString() {
+		return "Profesor " + super.toString() +  "Cargo = " + Cargo + " Antiguedad Docente = " + AntiguedadDocente;
+	}
+}
